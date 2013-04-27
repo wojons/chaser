cc818593915d6fc87b38c0895195f7c5b49780bf
Alexis Okuwa <wojonstech@gmail.com>
2013-04-27 01:01:23 -0700

    lets get this ready, working on dealing with turning web sockets into http calls

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tornado.ioloop
 import tornado.web
+import tornado.httpserver
 import inspect
 import importlib
 import os
@@ -10,10 +11,11 @@ import glob
 if __name__ == "__main__":
 	endPoints = {'http' : list(), 'sock' : dict()}
 	controllers = dict()
+	lib = dict()
 	
 	for i in glob.glob('./controllers/*.py'): #we dont need the pyc files
-		#print i
-		#fix file name
+		
+		#print i #fix file name
 		i = i.replace(".py", "")
 		i = i.replace("./controllers/", "")
 		
@@ -28,8 +30,14 @@ if __name__ == "__main__":
 			if not member[0].startswith("_"):
 				endPoints['http'].append((r"/api/http/"+i+"/"+member[0], getattr(m, member[0])))
 				#print member[0]
+		
+		#time for websockets
+		lib['websockstes'] = importlib.import_module('lib.websockets')
+		endPoints['sock'] = tornado.web.Application([(r'/api/websock', lib['websockstes'].WsHandler),]) #start the socket server
 	
 	print endPoints
-	application = tornado.web.Application(endPoints['http'])			
-	application.listen(54213)
+	http = tornado.web.Application(endPoints['http'])
+	sock = tornado.httpserver.HTTPServer(endPoints['sock'])
+	http.listen(54213)
+	sock.listen(8888)
 	tornado.ioloop.IOLoop.instance().start()
